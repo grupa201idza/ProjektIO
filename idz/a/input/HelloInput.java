163428ecdd7a0a03fce8c1533e4ac2ad122fbfb6
MaciejFaron <supremacey@icloud.com>
2014-04-03 22:34:14 +0200

    fixed  HelloInput

--- idz/a/input/HelloInput.java
+++ idz/a/input/HelloInput.java
@@ -4,7 +4,7 @@ public class HelloInput {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi Git");
+		System.out.println("Hello Git");
 	}
 
 }
