8e4e9d2e990aebcef446defa616ddd0ab7d77c8a
MaciejFaron <supremacey@icloud.com>
2014-04-03 22:33:25 +0200

    Fixed bugs in HelloCore

--- idz/a/core/HelloCore.java
+++ idz/a/core/HelloCore.java
@@ -5,6 +5,7 @@ public class HelloCore {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
+		System.out.println("Fixing some bugs");
 	}
 
 }
