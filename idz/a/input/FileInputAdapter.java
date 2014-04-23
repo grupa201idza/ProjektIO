0c9efed4db9e590dccd2b49add9ae8115b983d87
Przemyslaw Springer <przmk01@gmail.com>
2014-04-23 19:51:05 +0200

    File input adapter class

---
+++ idz/a/input/FileInputAdapter.java
@@ -0,0 +1,19 @@
+package adapter.input.file;
+
+import javax.security.auth.login.Configuration;
+import input.methods.InputAdapter;
+import input.methods.QueueManager;
+
+public class FileInputAdapter implements InputAdapter {
+
+	@Override
+	public void setupConfig(Configuration config) {
+		
+	}
+
+	@Override
+	public void connectToQueueManager(QueueManager queue) {
+		
+	}
+	
+}
