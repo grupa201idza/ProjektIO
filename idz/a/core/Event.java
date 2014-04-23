91f2e3e250914ae3cac507a3056cd99d0c269be3
Bartosz Domin <bdomin93@gmail.com>
2014-04-23 19:07:48 +0200

    Event class updated
    Signed-off-by: Bartosz Domin <bdomin93@gmail.com>

--- idz/a/core/Event.java
+++ idz/a/core/Event.java
@@ -1,10 +1,44 @@
 package idz.a.core;
 
 import java.sql.Timestamp;
-import java.util.Enumeration;
 
 public class Event {
-Timestamp timestamp;
-Enumeration logLevel;
-String details;
+	
+private Timestamp timestamp;
+private String details;
+private LogLevel loglevel;
+private enum LogLevel {
+	INFO,
+	WARNING,
+	ERROR,
+	SEVERE
+};
+
+public Timestamp getTimestamp() {
+	return timestamp;
+}
+
+public void setTimestamp(Timestamp timestamp) {
+	this.timestamp = timestamp;
+}
+
+public String getDetails() {
+	return details;
+}
+
+public void setDetails(String details) {
+	this.details = details;
+}
+
+public LogLevel getLoglevel() {
+	return loglevel;
+}
+
+public void setLoglevel(LogLevel loglevel) {
+	this.loglevel = loglevel;
+}
+
+public static void main(String[] args) {
+}
+
 }
