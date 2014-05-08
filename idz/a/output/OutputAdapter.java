c53c6f024e5a07dcb05c7acb9bfabbece04ff495
Pawel Jasik <paweljasik93@gmail.com>
2014-05-08 22:20:58 +0200

    partially implemented DBOutputAdapter

--- idz/a/output/OutputAdapter.java
+++ idz/a/output/OutputAdapter.java
@@ -1,5 +1,8 @@
 package idz.a.output;
 
+import java.util.List;
+import idz.a.core.Configuration;
+
 /*
  * OutputManagement serves just as a set of methods that need to be implemented in each OutputAdapter
  */
