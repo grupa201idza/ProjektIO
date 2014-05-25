0c811d77bbbdc77ca5230a3697b8bca9e3ffd68f
Bartosz Domin <bdomin93@gmail.com>
2014-05-25 14:28:00 +0200

    Configuration class and config file update
    Signed-off-by: Bartosz Domin <bdomin93@gmail.com>

--- project/idz/a/core/Configuration.java
+++ project/idz/a/core/Configuration.java
@@ -1,4 +1,3 @@
-
 package idz.a.core;
 
 import java.io.File;
@@ -6,31 +5,36 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
 public class Configuration {
-	
-	private static String inputAdapter, outputAdapter, inputFilePath, outputFilePath;
-	private static int inputSocket,batchSize;
+
+	private static String inputAdapter, outputAdapter, inputFilePath,
+			outputFilePath, inputURL, DBLogin, DBPassword, DBUrl, DBTable;
+	private static int inputSocket, batchSize, DBPort;
 	Properties prop = new Properties();
 	InputStream input = null;
 
-	
-	public Configuration(String path){
-		
-		
-		try {	
+	public Configuration(String path) {
+
+		try {
 			File conf = new File(path);
-			input = new FileInputStream(conf);	 
+			input = new FileInputStream(conf);
 			// load a config file
-			prop.load(input);	 
+			prop.load(input);
 			// read values
-			inputAdapter=prop.getProperty("InputAdapter");
-			outputAdapter=prop.getProperty("OutputAdapter");
-			inputFilePath=prop.getProperty("inputFilePath");
-			outputFilePath=prop.getProperty("outputFilePath");
-			batchSize=Integer.parseInt(prop.getProperty("BatchSize"));
-			inputSocket=Integer.parseInt(prop.getProperty("inputSocket"));
-		
-			
+			inputAdapter = prop.getProperty("InputAdapter");
+			outputAdapter = prop.getProperty("OutputAdapter");
+			inputFilePath = prop.getProperty("inputFilePath");
+			outputFilePath = prop.getProperty("outputFilePath");
+			batchSize = Integer.parseInt(prop.getProperty("BatchSize"));
+			inputSocket = Integer.parseInt(prop.getProperty("inputSocket"));
+			inputURL = prop.getProperty("inputURL");
+			DBLogin = prop.getProperty("DBLogin");
+			DBPassword = prop.getProperty("DBPassword");
+			DBPort = Integer.parseInt(prop.getProperty("DBPort"));
+			DBUrl = prop.getProperty("DBUrl");
+			DBTable = prop.getProperty("DBTable");
+
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} finally {
@@ -43,29 +47,53 @@ public class Configuration {
 			}
 		}
 	}
-	
-  public String getInputAdapter() {
+
+	public static String getInputAdapter() {
 		return inputAdapter;
 	}
 
-	public String getOutputAdapter() {
+	public static String getOutputAdapter() {
 		return outputAdapter;
 	}
 
-	public String getInputFilePath() {
+	public static String getInputFilePath() {
 		return inputFilePath;
 	}
 
-	public String getOutputFilePath() {
+	public static String getOutputFilePath() {
 		return outputFilePath;
 	}
 
-	public int getInputSocket() {
+	public static int getInputSocket() {
 		return inputSocket;
 	}
 
-	public int getBatchSize() {
+	public static String getInputURL() {
+		return inputURL;
+	}
+
+	public static int getBatchSize() {
 		return batchSize;
+	}
+
+	public static String getDBLogin() {
+		return DBLogin;
+	}
+
+	public static String getDBPassword() {
+		return DBPassword;
+	}
+
+	public static String getDBUrl() {
+		return DBUrl;
+	}
+
+	public static String getDBTable() {
+		return DBTable;
+	}
+
+	public static int getDBPort() {
+		return DBPort;
 	}
 
 }
