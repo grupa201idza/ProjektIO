492dcc15f8a1ae72f0b2fed4cfc065994c8f9005
Bartosz Domin <bdomin93@gmail.com>
2014-05-25 14:25:15 +0200

    Event class update
    Signed-off-by: Bartosz Domin <bdomin93@gmail.com>

--- project/idz/a/core/Event.java
+++ project/idz/a/core/Event.java
@@ -3,44 +3,44 @@ package idz.a.core;
 import java.sql.Timestamp;
 
 public class Event {
-	
-	public Event(Timestamp ts, String det, LogLevel ll){
-		timestamp=ts;
-		details=det;
-		loglevel=ll;
+
+	public Event(Timestamp ts, String det, Enum.LogLevel ll) {
+		timestamp = ts;
+		details = det;
+		loglevel = ll;
 	}
-	
-private Timestamp timestamp;
-private String details;
-private LogLevel loglevel;
-private enum LogLevel {
-	INFO,
-	WARNING,
-	ERROR,
-	SEVERE
-};
 
-public Timestamp getTimestamp() {
-	return timestamp;
-}
+	private Timestamp timestamp;
+	private String details;
+	private Enum.LogLevel loglevel;
 
-public void setTimestamp(Timestamp timestamp) {
-	this.timestamp = timestamp;
-}
+	public static class Enum {
+		private enum LogLevel {
+			INFO, WARNING, ERROR, SEVERE
+		};
+	}
 
-public String getDetails() {
-	return details;
-}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
 
-public void setDetails(String details) {
-	this.details = details;
-}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
 
-public LogLevel getLoglevel() {
-	return loglevel;
-}
+	public String getDetails() {
+		return details;
+	}
 
-public void setLoglevel(LogLevel loglevel) {
-	this.loglevel = loglevel;
-}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public Enum.LogLevel getLoglevel() {
+		return loglevel;
+	}
+
+	public void setLoglevel(Enum.LogLevel loglevel) {
+		this.loglevel = loglevel;
+	}
 }
