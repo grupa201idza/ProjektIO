6ecb20fafb5029551847ee7901cf0b5e96b91ece
Łukasz Kot <lukasz.kot@hotmail.com>
2014-05-25 16:52:36 +0200

    SocketInputAdapter minor changes

--- project/idz/a/input/SocketInputAdapter.java
+++ project/idz/a/input/SocketInputAdapter.java
@@ -10,6 +10,7 @@ public class SocketInputAdapter implements InputAdapter, Runnable{
 	Thread thread;
 	Configuration config;
 	QueueManager queue;
+	boolean running = false;
 	
 	public SocketInputAdapter(){
 		
@@ -20,9 +21,9 @@ public class SocketInputAdapter implements InputAdapter, Runnable{
 	public void run() {
 			ServerSocket serverSocket = null;
 			Socket socket = null;
-			
+			if (!running){			
 			try {
-				serverSocket = new ServerSocket(config.port);
+					serverSocket = new ServerSocket(config.inputSocket);
 			} catch (IOException e) {
 				System.out.println("Blad przy tworzeniu gniazda serwerowego");
 				System.exit(-1);
@@ -32,10 +33,13 @@ public class SocketInputAdapter implements InputAdapter, Runnable{
 			while (true){
 				try {
 					socket = serverSocket.accept();
+					running = true;
 				} catch (IOException e) {
+					running = false;
 					System.out.println("Blad wejscia-wyjscia");
 				}
 				new Thread(new SocketInputThread(socket, queue)).start();
+			}
 			}
 			
 	}
