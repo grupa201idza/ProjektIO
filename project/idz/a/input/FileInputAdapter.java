e18504bde4c3e2ee571149bdd5155a869a1500d6
Przemyslaw Springer <przmk01@gmail.com>
2014-06-01 18:00:44 +0200

    (Supposedly) complete FileInputAdapter

--- project/idz/a/input/FileInputAdapter.java
+++ project/idz/a/input/FileInputAdapter.java
@@ -1,8 +1,9 @@
 package idz.a.input;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import idz.a.core.Configuration;
+import idz.a.core.Event;
+import idz.a.core.QueueManager;
+
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -11,34 +12,29 @@ import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.util.Scanner;
 
-import idz.a.core.Configuration;
-import idz.a.core.Event;
-import idz.a.core.QueueManager;
-
 
 public class FileInputAdapter implements InputAdapter {
 
 	private Configuration config;
 	private QueueManager queue;
-	private String logFile = new String("D:\\server.log.1.txt"); // roboczy path
 	private String logPath;
 	private final static Charset ENCODING = StandardCharsets.UTF_8;
-	Event event;
+	private Event event;
 	
 	public FileInputAdapter() {
 		super();
 	}
 	
-	/** Constructor */
+	/** Constructor of FileInputAdapter */
 	public FileInputAdapter(Configuration config, QueueManager queue) {
 		setupConfig(config);
 		connectToQueueManager(queue);
-		logPath = config.getInputFilePath();
+		logPath = Configuration.getInputFilePath();
 		
 	}
 	
 	/** Method returning Path from given String path
-	 *  @param fileName - full name of an existing, readable file */
+	 *  @param fileName full name of an existing, readable file */
 	private Path getPath(String fileName) {
 	    return Paths.get(fileName);
 	}
@@ -48,7 +44,7 @@ public class FileInputAdapter implements InputAdapter {
 		int connectionAttempts = 0;
 		while (connectionAttempts < 5) {
 			try {
-				Scanner sc = new Scanner(getPath(logFile), ENCODING.name());
+				Scanner sc = new Scanner(getPath(logPath), ENCODING.name());
 				sc.close();
 			} catch (IOException e) {
 				System.out.println("Attempting to connect to source");
@@ -61,14 +57,14 @@ public class FileInputAdapter implements InputAdapter {
 	}
 	
 	/** Method processing log file line by line 
-	 * @throws IOException - Exception caught in connectToSource() */
+	 * @throws IOException Exception caught in connectToSource() */
 	private void processLogFile() throws IOException {
 		if (connectToSource()) {
-			Scanner scanner =  new Scanner(getPath(logFile), ENCODING.name());
+			Scanner scanner =  new Scanner(getPath(logPath), ENCODING.name());
 			// any log left? is there space in queue?
-			while (scanner.hasNextLine() && queue.currentSize() < queue.batchSize){
+			while (scanner.hasNextLine() && queue.currentSize() < Configuration.getBatchSize()){
 				event = parseEvent(scanner.nextLine());
-				if (!event.equals(null)) {
+				if (!event.equals(null) && !event.getTimestamp().equals(null) && !event.getDetails().equals(null) && !event.getLoglevel().equals(null)) {
 					queue.acceptEvent(event);
 				}
 				else continue;
@@ -82,28 +78,40 @@ public class FileInputAdapter implements InputAdapter {
 	}
 	
 	/** Method for parsing line's content to Event
-	 *  @param log - scanned line from log file */
-	private Event parseEvent(String log){
+	 *  @param log scanned line from log file */
+	public Event parseEvent(String log){
 		Scanner scanner = new Scanner(log);
 		scanner.useDelimiter(" ");
 		if (scanner.hasNext()){
-			String date = scanner.next();
-			Timestamp ts = Timestamp.valueOf(timeFormat(date));
-			System.out.println("Timestamp: " +ts);
-			
-			String level = scanner.next();
-			Event.Enum.LogLevel loglvl = Event.Enum.LogLevel.valueOf(level);
-			System.out.println(loglvl);
-			
-			String detail = new String();
-			while (scanner.hasNext()) {
-				detail = detail.concat(" " +scanner.next());
+			Event event = new Event(null, null, null);
+			try {
+				String date = scanner.next();
+				event.setTimestamp(Timestamp.valueOf(timeFormat(date)));
+			} catch (IllegalArgumentException e) {
+				System.err.println("Wrong Timestamp");
+				event.setTimestamp(null);
 			}
-			detail = detail.trim();
-			System.out.println("Detail: " +detail.trim());
 			
+			try {
+				String level = scanner.next();
+				event.setLoglevel(Event.Enum.LogLevel.valueOf(level));;
+			} catch (IllegalArgumentException e) {
+				System.err.println("Wrong Loglevel");
+				event.setLoglevel(null);
+			}
+			
+			try {
+				String detail = new String();
+				while (scanner.hasNext()) {
+					detail = detail.concat(" " +scanner.next());
+				}
+				event.setDetails(detail.trim());
+			} catch (IllegalArgumentException e) {
+				System.err.println("Wrong Detail");
+				event.setDetails(null);
+			}
 			scanner.close();
-			return new Event(ts, detail, loglvl);
+			return event;
 		}
 		else {
 			System.out.println("Empty or invalid line. Unable to process.");
@@ -113,24 +121,17 @@ public class FileInputAdapter implements InputAdapter {
 	}
 	
 	/** Method formating log date
-	 *  @param in - date to format */
+	 *  @param in date to format */
 	private String timeFormat (String in) {
 		return in.replace("(", "").replace(")","").replace("T", " ");
 	}
 	
-	@Override
 	public void setupConfig(Configuration config) {
 		this.config = config;
 	}
-
-	@Override
+	
 	public void connectToQueueManager(QueueManager queue) {
 		this.queue = queue;
 	}
-	
-	
-	public static void main(String[] args) throws IOException {
-		FileInputAdapter f = new FileInputAdapter();
-		f.processLogFile();
-	}
+
 }
