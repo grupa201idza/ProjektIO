206057fbfac474aba5e3ed586d5df50e9ff9bad0
Łukasz Kot <lukasz.kot@hotmail.com>
2014-06-05 20:18:47 +0200

    SocketInputAdapter check styler cleanup

--- project/idz/a/input/SocketInputAdapter.java
+++ project/idz/a/input/SocketInputAdapter.java
@@ -13,20 +13,15 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class SocketInputAdapter implements InputAdapter{
-
-	Configuration config;
-	QueueManager queue;
-	Socket socket;
-	
-	public SocketInputAdapter(Socket socket, QueueManager queue)
-	{
+public class SocketInputAdapter implements InputAdapter {
+		Configuration config;
+		QueueManager queue;
+		Socket socket;
+		public SocketInputAdapter(Socket socket, QueueManager queue) {
 		this.socket = socket;
 		this.queue = queue;
 	}
-	
-	private void parseEvent(String log){
-		
+	private void parseEvent(final String log) {
 		String tDate;
 		String tTime;
 		String tLevel;
@@ -34,69 +29,73 @@ public class SocketInputAdapter implements InputAdapter{
 		Timestamp timestamp = null;
 		Event.Enum.LogLevel logLevel = null;
 		Event event = null;
-		
-		if (log.length()>=32)
-		{
-			tDate = log.substring(1, 11);
-			tTime = log.substring(12, 25);
+		final int logLength = 32;
+		final int tDateSubsSec = 11;
+		final int tTimeSubsFir = 12;
+		final int tTimeSubsSec = 25;
+		if (log.length() >= logLength) {
+			tDate = log.substring(1, tDateSubsSec);
+			tTime = log.substring(tTimeSubsFir, tTimeSubsSec);
 			tDate = tDate + " " + tTime;
-			
-			
-			DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+			DateFormat formatter = new
+SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 			try {
 				Date parsedDate = formatter.parse(tDate);
 				timestamp = new Timestamp(parsedDate.getTime());
 			} catch (ParseException e) {
 				return;
 			}
-			
-			tLevel = log.substring(27, 28);
-			
+			final int tLevelSubsFir = 27;
+			final int tLevelSubsSec = 28;
+			tLevel = log.substring(tLevelSubsFir, tLevelSubsSec);
+			int positionDetails = 0;
+			final int posDetInf = 32;
+			final int posDetWar = 35;
+			final int posDetErr = 33;
+			final int posDetSev = 34;
 			switch (tLevel) {
-			case "I": 
+			case "I":
 				logLevel = Event.Enum.LogLevel.INFO;
-				tDetails = log.substring(32);
+				positionDetails = posDetInf;
 				break;
-			case "W": 
+			case "W":
 				logLevel = Event.Enum.LogLevel.WARNING;
-				tDetails = log.substring(35);
+				positionDetails = posDetWar;
 				break;
-			case "E": 
+			case "E":
 				logLevel = Event.Enum.LogLevel.ERROR;
-				tDetails = log.substring(33);
+				positionDetails = posDetErr;
 				break;
-			case "S": 
+			case "S":
 				logLevel = Event.Enum.LogLevel.SEVERE;
-				tDetails = log.substring(34);
+				positionDetails = posDetSev;
+				break;
+			default: System.out.println("Error");
 				break;
 			}
+			tDetails = log.substring(positionDetails);
 			event = new Event(timestamp, tDetails, logLevel);
-			if (queue.acceptEvent(event))
-				System.out.println("Added to queue.");
-			else
-				System.out.println("Not added to queue.");
+			if (queue.acceptEvent(event)) { System.out.println("Added to queue."); } else { System.out.println("Not added to queue."); }
 		}
 	}
-	
-	public void start() {
-		
+	final void start() {
 		BufferedReader brinp = null;
-		
 		try {
-			brinp = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			brinp = new BufferedReader(
+new InputStreamReader(socket.getInputStream()));
 		} catch (IOException e) {
-			System.out.println("Blad przy tworzeniu strumienia " + e);
+			System.out.println(
+"Blad przy tworzeniu strumienia " + e);
 		}
 		String line = null;
-		
-		while(true){
-		try{
+		while (true) {
+		try {
 			line = brinp.readLine();
-			if (line != null){
+			if (line != null) {
 				parseEvent(line);
 			}
-		}
-		catch (IOException e){
+}
+		catch (IOException e) {
 			System.out.println("Blad wejscia-wyjscia");
 			return;
 		}
@@ -111,7 +110,5 @@ public class SocketInputAdapter implements InputAdapter{
 	public void connectToQueueManager(QueueManager queue) {
 		// TODO Auto-generated method stub
 		this.queue = queue;
-		
 	}
-
 }
