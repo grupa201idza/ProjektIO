819222d1c20d00a7c9f1beb2605446e9cb1aeeb3
govgami <hadrime@o2.pl>
2014-06-09 08:54:24 +0200

    actualised paths espc. Event

--- src/main/java/idz/a/output/OutputAdapter.java
+++ src/main/java/idz/a/output/OutputAdapter.java
@@ -1,8 +1,8 @@
-package idz.a.output;
+package src.main.java.idz.a.output;
 
 import java.util.List;
-import idz.a.core.Configuration;
-import idz.a.core.Event;
+import src.main.java.idz.a.core.Configuration;
+import src.main.java.idz.a.core.Event;
 
 /*
  * OutputManagement serves just as a set of methods that need to be implemented in each OutputAdapter
