4b06dec9f3640aa55c14af01ad259d5add8f46f0
Sławomir <sirslaw@gmail.com>
2014-06-09 20:17:50 +0200

    Deleting incorrect documentation

--- src/main/java/idz/a/core/AppCore.java
+++ src/main/java/idz/a/core/AppCore.java
@@ -4,20 +4,10 @@ import idz.a.input.InputAdapter;
 import idz.a.output.OutputAdapter;
 
 /**
- * @author Bartosz Domin 
+ * @author Bartosz Domin
  * 
  */
-public class dokumentacjaAppCore{
-	
 
-/** 
- *@autor Kamil Klarecki 
- *  Rdzeñ funkcjonalny aplikacji.
- *  Przechowuje obiekty konfiguracji i adapterów.
- *  Posiada ³añcuch œcie¿ki inicjalizuj¹cej.
- *  */
-	
-}
 public class AppCore {
 
 	static String configPath = "project/idz/a/core/Config.txt";
@@ -27,35 +17,14 @@ public class AppCore {
 	static QueueManager queue;
 	int batchSize = 0;
 	static String inputName, outputName;
-	
-public class dokumentacjaAppCore(String path){
 
-		/** 
-		 *@autor Kamil Klarecki 
-		 *  Konstruktor przyjmuj¹cy za parametr œciêzkê pliku incjalizucego 
-		 *  konfiguracji.
-		 *  Two¿y now¹ konfiguracje w oparciu o podan¹ scie¿kê.
-		 *  Na podstawie konfiguraci ustala rozmiar kolejki logów oraz wejœciowy i 
-		 *  wyjœciowy adapter wed³ug nazwy.
-		 *  */
-			
-		}
 	public AppCore(String path) {
 		conf = new Configuration(path);
 		batchSize = conf.getBatchSize();
 		inputName = conf.getInputAdapter();
 		outputName = conf.getOutputAdapter();
 	}
-	public class dokumentacjaloadInputAdapter{
-		
 
-		/** 
-		 *@autor Kamil Klarecki 
-		 *  *  Tworzy obiekt adaptera implementuj¹cego interface 
-		 *  InputAdapter o zadanej nazwie
-		 *  */
-			
-		}
 	private static void loadInputAdapter(String name) {
 
 		try {
@@ -67,17 +36,6 @@ public class dokumentacjaAppCore(String path){
 			e.printStackTrace();
 		}
 	}
-	
-	public class dokumentacjaloadOutputAdapter{
-		
-
-		/** 
-		 *@autor Kamil Klarecki 
-		 *  *  Tworzy obiekt adaptera implementuj¹cego interface 
-		 * Output InputAdapter o zadanej nazwie
-		 *  */
-			
-		}
 
 	private static void loadOutputAdapter(String name) {
 
@@ -91,47 +49,19 @@ public class dokumentacjaAppCore(String path){
 		}
 
 	}
-	public class dokumentacjasetUp{
-		
 
-		/** 
-		 *@autor Kamil Klarecki 
-		 *  Wyko¿ystuje metody ³aduj¹ce adapter wejœciowy i wyjsciowy 
-		 *  ze œcie¿ki o zadanej nazwie i powoduje now¹ instancjê menad¿era kolejki
-		 *  */
-			
-		}
 	private static void setUp() {
 		loadInputAdapter("idz.a.input." + inputName);
 		loadOutputAdapter("idz.a.output." + outputName);
 		queue = new QueueManager();
 	}
-public class dokumentacjainvokeAdapterMethods{
-		
 
-		/** 
-		 *@autor Kamil Klarecki 
-		 *  Inicjalizuje pola adapter wyko¿ystuj¹c istniej¹c¹ konfiruracjê
-		 *  Do³¹cza obiekt menad¿era kolejki incjalizuje pola adaptera wyjœciowego
-		 *  */
-			
-		}
 	private static void invokeAdapterMethods() {
 		in.setupConfig(conf);
 		in.connectToQueueManager(queue);
 		out.setupConfig(conf);
 	}
-public class dokumentacjaMain{
-		
 
-		/** 
-		 *@autor Kamil Klarecki 
-		 *  Tworzy obiekt ApCore
-		 *  Wywo³uje  setUp i invokeAdapterMethod
-		 *  oraz wcztuje logi w nieskoñczonej pêtli
-		 *  */
-			
-		}
 	public static void main(String[] args) {
 		new AppCore(configPath);
 		setUp();
