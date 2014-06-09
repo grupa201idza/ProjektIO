332a4c527786d46ac7520bd3b8c92ca4949eb9a9
ginmax <admin@Ola-Komputer>
2014-06-09 12:59:59 +0200

    Dokumentacja

--- src/main/java/idz/a/core/Event.java
+++ src/main/java/idz/a/core/Event.java
@@ -6,6 +6,19 @@ import java.sql.Timestamp;
  * @author Bartosz Domin
  * 
  */
+}
+
+public class dokumentacjaEvent{
+
+	/** 
+	 *@autor Kamil Klarecki 
+	 *  Przechowuje informacje o czasie, szczegó³ach, 
+	 *  rodzaju loga
+	 *  
+	 *  */
+
+}
+
 
 public class Event {
 
@@ -18,33 +31,82 @@ public class Event {
 	private Timestamp timestamp;
 	private String details;
 	private Enum.LogLevel loglevel;
+	
+	public class dokumentacjaEnum{
 
+		/** 
+		 *@autor Kamil Klarecki 
+		 *  przechowuje typ loga:
+		 *   informacjê,ostrze¿enie, b³¹d, naruszenie 
+		 *  */
+	
 	public static class Enum {
 		public enum LogLevel {
 			INFO, WARNING, ERROR, SEVERE
 		};
 	}
 
+	public class dokumentacjaTimestamp getTimestamp(){
+
+		/** 
+		 *@autor Kamil Klarecki 
+		 *  
+		 *  */
+		
 	public Timestamp getTimestamp() {
 		return timestamp;
 	}
+	
+	public class dokumentacjasetTimestamp{
 
+		/** 
+		 *@autor Kamil Klarecki 
+		 *  Zwraca znacznik czasowy
+		 *  
+		 *  
+		 *  */
 	public void setTimestamp(Timestamp timestamp) {
 		this.timestamp = timestamp;
 	}
+	public class dokumentacjasetTimestamp{
 
+	/** 
+	 *@autor Kamil Klarecki 
+	 *  Przypisuje znacznik czasowy
+	 *  
+	 *  
+	 *  */
 	public String getDetails() {
 		return details;
 	}
 
+	/** 
+	 *@autor Kamil Klarecki 
+	 *  Zswraca ³añcuch zawieraj¹cy szczegó³y loga
+
+	 *  */
 	public void setDetails(String details) {
 		this.details = details;
 	}
 
+	/** 
+	 *@autor Kamil Klarecki 
+	 *  Zwracarodzaj loga
+	 *  
+	 *  */
 	public Enum.LogLevel getLoglevel() {
 		return loglevel;
 	}
 
+
+	
+	public class dokumentacjsetLoglevel{
+
+		/** 
+		 *@autor Kamil Klarecki 
+		 *  Zwraca poziom loga
+		 *  
+		 *  */
 	public void setLoglevel(Enum.LogLevel loglevel) {
 		this.loglevel = loglevel;
 	}
