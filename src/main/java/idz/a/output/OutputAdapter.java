b1cb627fc125bfb4cd200c9702c43d60db34a81e
govgami <zz-ad@localhost>
2014-06-09 09:31:12 +0200

    actualisation: OA : imp Event FOA : parsing adjusted + ForeseenExeption lost in early repo transformation served by Exception

--- src/main/java/idz/a/output/OutputAdapter.java
+++ src/main/java/idz/a/output/OutputAdapter.java
@@ -1,8 +1,8 @@
-package src.main.java.idz.a.output;
+package idz.a.output;
 
 import java.util.List;
-import src.main.java.idz.a.core.Configuration;
-import src.main.java.idz.a.core.Event;
+import idz.a.core.Configuration;
+import idz.a.core.Event;
 
 /*
  * OutputManagement serves just as a set of methods that need to be implemented in each OutputAdapter
