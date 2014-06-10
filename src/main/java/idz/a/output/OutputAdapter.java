d7b9bda2b1bb29c3a26b532535f4d35d739f4d31
ginmax <admin@Ola-Komputer>
2014-06-10 14:28:58 +0200

    Dokumentacja

--- src/main/java/idz/a/output/OutputAdapter.java
+++ src/main/java/idz/a/output/OutputAdapter.java
@@ -7,7 +7,9 @@ import idz.a.core.Event;
 /*
  * OutputManagement serves just as a set of methods that need to be implemented in each OutputAdapter
  */
-
+/**
+ * Interfejs zawieraj¹cy zestaw metod które musz¹ byæ zrealizowane w kazdym adapterze wyjsciowym
+ */
 public interface OutputAdapter {
 public void setupConfig(Configuration config);
 public boolean storeEvents(List<Event> batch);
