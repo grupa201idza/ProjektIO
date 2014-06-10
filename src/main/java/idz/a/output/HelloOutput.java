a378fc180b4cdab98f94b9cf5a9740ddbaa4cd5a
ginmax <admin@Ola-Komputer>
2014-06-10 15:00:20 +0200

    Dokumentacja

--- src/main/java/idz/a/output/HelloOutput.java
+++ src/main/java/idz/a/output/HelloOutput.java
@@ -1,7 +1,9 @@
 package idz.a.output;
 
 public class HelloOutput {
-
+/**
+ * Interfejs zawieraj¹cy zestaw metod które musz¹ byæ zrealizowane w kazdym adapterze wyjsciowym
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
