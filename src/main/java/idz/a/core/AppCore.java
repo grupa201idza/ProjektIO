0e4758ccd577384cd045088f5819c3528f486300
ginmax <admin@Ola-Komputer>
2014-06-10 19:18:12 +0200

    Dokumentacja

--- src/main/java/idz/a/core/AppCore.java
+++ src/main/java/idz/a/core/AppCore.java
@@ -17,11 +17,13 @@ public class AppCore {
 	static QueueManager queue;
 	int batchSize = 0;
 	static String inputName, outputName;
+
 	/**
-		 * Konstruktor przyjmujｹcy za parametr 彡ieｿk� pliku inicjalizujｹcego konfiguracji.
-		 * Tworzy nowｹ konfiguracje w oparciu o podanｹ 彡iezke.
-		 * Na podstawie konfiguracji ustala rozmiar kolejki log oraz wej彡iowy i wyj彡iowy 
-		 * adapter wedｳug nazwy.
+	 * Konstruktor przyjmujacy za parametr sciezke pliku inicjalizujacego
+	 * konfiguracji. Tworzy now� konfiguracje w oparciu o podana sciezke. Na
+	 * podstawie konfiguracji ustala rozmiar kolejki logow oraz wejsciowy i
+	 * wyjsciowy adapter wedlug nazwy.
+	 * 
 	 * @param path
 	 */
 	public AppCore(String path) {
@@ -30,11 +32,12 @@ public class AppCore {
 		inputName = conf.getInputAdapter();
 		outputName = conf.getOutputAdapter();
 	}
-	
+
 	/**
-	 * Tworzy obiekt adaptera implementujｹcego interface 
-	 *  InputAdapter o zadanej nazwie.
-	 * @param name nazwa interfejsu
+	 * Tworzy obiekt adaptera implementujacego interface InputAdapter o
+	 * zadanej nazwie.
+	 * 
+	 * @param name  nazwa interfejsu
 	 */
 	private static void loadInputAdapter(String name) {
 
@@ -47,10 +50,11 @@ public class AppCore {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 *    Tworzy obiekt adaptera implementujｹcego interfejs
-	 * Output InputAdapter o zadanej nazwie
+	 * Tworzy obiekt adaptera implementujacego interfejs Output InputAdapter o
+	 * zadanej nazwie
+	 * 
 	 * @param name nazwa parametru
 	 */
 	private static void loadOutputAdapter(String name) {
@@ -65,31 +69,33 @@ public class AppCore {
 		}
 
 	}
-	/** 
-	 * Wykorzystuje metody ｳadujｹce adapter wej彡iowy i wyj彡iowy 
-	 *  ze 彡ieｿki o zadanej nazwie i powoduje nowｹ instancj� menadｿera kolejki
-	 *  */
+
+	/**
+	 * Wykorzystuje metody ladujace adapter wejsciowy i wyjsciowy ze
+	 * sciezki o zadanej nazwie i powoduje nowa instancj� instancj� menadzera 
+	 * kolejki
+	 * */
 	private static void setUp() {
 		loadInputAdapter("idz.a.input." + inputName);
 		loadOutputAdapter("idz.a.output." + outputName);
 		queue = new QueueManager();
 		queue.setBatchSize(conf.getBatchSize());
 	}
-	
+
 	/**
-	 * Inicjalizuje pola adapter wykorzystujｹc istniejacｹ konfiguracje
-	 *  Dolacza obiekt menadzera kolejki inicjalizuje pola adaptera wyjsciowego
+	 * Inicjalizuje pola adapter wykorzystujac istniejaca konfiguracje
+	 * Dolacza obiekt menadzera kolejki inicjalizuje pola adaptera wyjsciowego
 	 */
 	private static void invokeAdapterMethods() {
 		in.setupConfig(conf);
 		in.connectToQueueManager(queue);
 		out.setupConfig(conf);
 	}
-	
+
 	/**
-	 * Tworzy obiekt ApCore
-	 *  Wywoluje  setUp i invokeAdapterMethod
-	 *  oraz wczytuje logi w nieskonczonej petl
+	 * Tworzy obiekt ApCore Wywoluje setUp i invokeAdapterMethod oraz wczytuje
+	 * logi w nieskonczonej petl
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
